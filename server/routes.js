64bbb1468c3ba13a49336ed036a1b4be90030f99
Lennard Rottjers <Lennard@rottjers.nl>
2019-04-19 23:13:19 +0200

    Fixed ESLint errors.

--- server/routes.js
+++ server/routes.js
@@ -168,7 +168,7 @@ router.get('*', async (ctx, next) => {
 });
 
 // middleware
-if(!!process.env.LDAP_REVERSE_PROXY_HEADER){
+if (!!process.env.LDAP_REVERSE_PROXY_HEADER) {
   koa.use(ldapReverseProxy());
 }
 koa.use(apexRedirect());
