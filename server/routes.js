4e0d75234309c52e69c558d841d280a6b8a72dd3
Lennard Rottjers <Lennard@rottjers.nl>
2019-04-19 22:11:03 +0200

    Basic LDAP sigin working.

--- server/routes.js
+++ server/routes.js
@@ -126,6 +126,7 @@ router.get('/', async ctx => {
           lastSignedIn={lastSignedIn}
           googleSigninEnabled={!!process.env.GOOGLE_CLIENT_ID}
           slackSigninEnabled={!!process.env.SLACK_KEY}
+          ldapSiginEnabled={!!process.env.LDAP_ENABLED}
           hostname={ctx.request.hostname}
         />
       );
@@ -143,6 +144,7 @@ router.get('/', async ctx => {
       lastSignedIn={lastSignedIn}
       googleSigninEnabled={!!process.env.GOOGLE_CLIENT_ID}
       slackSigninEnabled={!!process.env.SLACK_KEY}
+      ldapSigninEnabled={!!process.env.LDAP_ENABLED}
     />
   );
 });
